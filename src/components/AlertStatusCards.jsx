dd8189afdbe6b95086d7ac8c142b3be8de30384a
sbm0204 <59056231+sbm0204@users.noreply.github.com>
2025-10-10 20:53:42 +0900

    Create AlertStatusCards.jsx

---
+++ src/components/AlertStatusCards.jsx
@@ -0,0 +1,29 @@
+import './AlertStatusCards.css';
+
+function AlertStatusCards() {
+
+ return (
+    <><div>
+        <h1 className='title'>미세먼지 경보 현황</h1>
+        <div className='alert-status-cards-container'>
+            <div className='alert-status-card'>
+                <div className='alert-status-card-districtName'>서울</div>
+                <div className='alert-status-card-contents'>
+                    <div>
+                    <p>발령: 100 µg/m³ [나쁨]</p>
+                    <p>2025-09-26 12:00</p>
+                    </div>
+                    <div>
+                    <p>해제: 50 µg/m³ [보통]</p>
+                    <p>2025-09-26 15:00</p>
+                    </div>
+                </div>
+                <div className='alert-status-card-issueGbn'>주의보</div>
+            </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default AlertStatusCards;
