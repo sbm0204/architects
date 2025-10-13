6e4215f254702222a90c5ae350a16c279d2e2330
Kimsangwon-093 <akg9380@daum.net>
2025-10-13 19:34:59 +0900

    251013 소개페이지(반응형 미완성)

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,63 +1,36 @@
 import './Service.css';
-import icon1 from '../../assets/Home-logo.png';
-import icon2 from '../../assets/Caution.png';
-import icon3 from '../../assets/People.png';
-import icon4 from '../../assets/Lung.png';
+import TeamImageWeb from "/src/assets/teamimageweb.png";
+import MisemeonjiImageWeb from "/src/assets/misemeonjiimageweb.png";
 
 function Service() {
   return (
-      <div className='contents-size'>
-        <div className="service-container">
-          <h1 className='service-title'>서비스 소개</h1>
+      <div className="container">
+        <h1 className="title">서비스 소개</h1>
+          {/* ------------------------------ */}
+        <div className="team-info">
+          <img src={TeamImageWeb} alt="팀소개" />
+          <div className="info-text">
+            <h2>깨끗한 세상을 향한 우리의 발걸음</h2>
+            <div className="underline"></div>
+            <div>
+              <p>우리 Architects 팀은 기술과 협력을 통해 더 나은 환경을 만들고자 합니다.</p>
+              <p>작은 아이디어 하나에서 시작된 이 프로젝트는 모두가 안심하고 숨 쉴 수 있는 </p>
+              <p>세상을 위한 첫걸음 입니다.</p>
+            </div>
+          </div>
         </div>
-        <div className="service-cardGrid">
-{/* -------------------------------------------------------- */}
-          <div className="card">
-            <div className='card-icon-box'>
-              <img src={icon1} className="card-icon" />
-            </div>
-            <div className='card-title-box'>
-              <h2 className="card-title underline-medium-purple">메인 페이지</h2>
-            </div>
-            <p>미세먼지 농도</p>
-            <p>상황별 행동지침</p>
+          {/* ------------------------------ */}
+        <div className="misemeonji">
+          <div className="info-text">
+            <h2>정확한 정보로 지키는 우리의 하루</h2>
+            <div className="underline"></div>
+            <p>본 사이트는 실시간 미세먼지 농도와 대기질 정보를 제공합니다.</p>
+            <p>데이터 기반의 신뢰할 수 있는 정보를 통해 사용자는 자신의 건강을 지키고</p>
+            <p>환경문제에 대한 인식을 높일수 있습니다.</p>
           </div>
-{/* -------------------------------------------------------- */}
-          <div className="card">
-            <div className="card-icon-box">
-              <img src={icon2} className="card-icon" />
-            </div>
-            <div className="card-title-box">
-              <h2 className="card-title underline-medium-purple">경보 페이지</h2>
-            </div>
-            <p>지역별 미세먼지 정보</p>
-            <p>( 주의 / 경고 / 위험)</p>
-          </div>
-{/* -------------------------------------------------------- */}
-          <div className="card">
-            <div className="card-icon-box">
-              <img src={icon3} className="card-icon" />
-            </div>
-            <div className="card-title-box">
-              <h2 className="card-title underline-medium-purple">행동요령 페이지</h2>
-            </div>
-            <p>색상별 위험수준</p>
-            <p>[ 좋음 / 보통 / 나쁨 / 매우 나쁨]</p>
-          </div>
-{/* -------------------------------------------------------- */}
-          <div className="card">
-            <div className="card-icon-box">
-              <img src={icon4} className="card-icon" />
-            </div>
-            <div className="card-title-box">
-              <h2 className="card-title underline-medium-purple">만든이 소개</h2>
-            </div>
-            <p>만든 사람들 소개</p>
-            <p>공기 프랜즈 소개</p>
-          </div>
-{/* -------------------------------------------------------- */}
+          <img src={MisemeonjiImageWeb} alt="미세먼지" />
         </div>
-      </div>
+       </div>
   )
 }
 
