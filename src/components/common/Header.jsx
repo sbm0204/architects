d9fc7cf1e2a1f5b0a367e3d3583104f4e1e552c3
kimjunghyun <john0210@hanmail.net>
2025-10-14 18:47:26 +0900

    251014 헤더 푸터 햄버거 버튼 클릭 전까지 전체적으로 적용

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,65 +1,51 @@
 import './Header.css';
 import logonew from '../../assets/logonew.png'; //로고 임포트
-import bgImage from '../../assets/bgimg-4.jpg'; //배경 이미지 임포트
-import homeIcon from'../../assets/iconhome.png'; //홈 아이콘 임포트
+import bgImage from '../../assets/background-photo-gradation.png'; //배경 이미지 임포트
+
 
 
 function Header() {
 
  return (
     <>
-
-   {/* PC 헤더 */}
-    <div className='header-pc'>
-      <div className='header-box'>
-        <div className='header-box-contents'>          
-          <div className='header-box-contents-bgimg' style={{backgroundImage: `url(${bgImage})`}}></div>
-          <nav className='nav'>
-            {/* 좌측 메뉴 */}
-            <ul className='nav-left'>
-              <li><img src={homeIcon} alt="홈아이콘" /></li>
-              <li>미세먼지 경보</li>
-            </ul>
-
-            {/*로고 가운데*/}
-            <div className='nav-logo'>
-              <img src={logonew} alt="로고" />
-            </div>
-
-            {/* 우측 메뉴 */}
-            <ul className='nav-right'>
-              <li>행동요령</li>
-              <li>서비스 소개</li>
-            </ul>         
-          </nav>
+      {/* PC 헤더 */}
+      <div className='header-pc'> 
+        <div className='header-box'>
+          <div className='header-background' style={{backgroundImage: `url(${bgImage})`}}>
+            <div className='header-contents'>
+              <div className='header-logo'>
+                <img src={logonew} alt="헤더 로고" />
+              </div>
+              <div className='header-nav'>
+                <div className='header-nav-contents'>미세먼지 경보</div>
+                <div class="nav-line">|</div>
+                <div className='header-nav-contents'>행동요령</div>
+                <div class="nav-line">|</div>
+                <div className='header-nav-contents'>서비스 소개</div>
+              </div>
+            </div>  
+          </div>
         </div>
-      </div>
-   </div>
-  {/* ----- 모바일 헤더 -----  */}
+      </div> 
 
+      {/* Mobile 헤더 */}
       <div className='header-mobile'>
-
         <div className='header-mobile-box'>
-          <div className='header-mobile-box-contents'>
-            <div className='header-mobile-box-contents-bgimg' style={{backgroundImage: `url(${bgImage})`}}></div> 
-            <div className='header-left'>
-              <img src={logonew} alt="모바일 로고" />
-            </div>
-            <div className='header-right'>
-              <div className='hamburger'>
+          <div className='header-mobile-background' style={{backgroundImage: `url(${bgImage})`}}>
+            <div className='header-mobile-contents'>
+              <img src={logonew} alt="모바일 헤더 로고" />
+              <div className='header-mobile-hamburger'>
                 <span></span>
                 <span></span>
                 <span></span>
               </div>
             </div>
           </div>
-       </div>
+        </div> 
       </div>
     </> 
   );
 }
-
-
 
 
 export default Header;
