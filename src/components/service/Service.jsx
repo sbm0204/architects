028494ea42bbdc93ccf38d56d716f07931b2641b
Kimsangwon-093 <akg9380@daum.net>
2025-10-16 18:20:48 +0900

    251016 반응형 웹 - 유지작업

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -10,14 +10,14 @@ const Service = () => {
           <img src={teamImageWeb} alt="팀 이미지" />
         </div>
         <div className="desktop-text-box">
-          <h3>깨끗한 세상을 향한 우리의 발걸음</h3>
+          <h2>깨끗한 세상을 향한 우리의 발걸음</h2>
           <div className="underline"></div>
           <p>우리는 기술과 협력을 통해 더 나은 환경을 만들고자 합니다.</p>
           <p>작은 아이디어 하나에서 시작된 이 프로젝트는 모두가 안심하고</p>
           <p>숨 쉴 수 있는 세상을 위한 첫걸음 입니다.</p>
         </div>
         <div className="mobile-text-box">
-          <h3>깨끗한 세상을 향한<br></br>우리의 발걸음</h3>
+          <h2>깨끗한 세상을 향한<br></br>우리의 발걸음</h2>
           <div className="underline"></div>
           <p>우리는 기술과 협력을 통해</p>
           <p>더 나은 환경을 만들고자 합니다.</p>
@@ -31,14 +31,14 @@ const Service = () => {
           <img src={misemeonjiImageWeb} alt="마스크 이미지" />
         </div>
         <div className="desktop-text-box">
-          <h3>정확한 정보로 지키는 우리의 하루</h3>
+          <h2>정확한 정보로 지키는 우리의 하루</h2>
           <div className="underline"></div>
           <p>본 사이트는 실시간 미세먼지 농도와 대기질 정보를 제공합니다.</p>
-          <p>데이터 기반의 신뢰할 수 있는 정보를 통해 사용자는 자신의 건강을 지키고</p>
-          <p>환경문제에 대한 인식을 높일수 있습니다.</p>
+          <p>데이터 기반의 신뢰할 수 있는 정보를 통해 사용자는</p>
+          <p>자신의 건강을 지키고 환경문제에 대한 인식을 높일수 있습니다.</p>
         </div>
         <div className="mobile-text-box">
-          <h3>정확한 정보로 지키는<br></br>우리의 하루</h3>
+          <h2>정확한 정보로 지키는<br></br>우리의 하루</h2>
           <div className="underline"></div>
           <p>본 사이트는 실시간 미세먼지 농도와 </p>
           <p>대기질 정보를 제공합니다.</p>
